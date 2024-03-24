bee7eb41d2b285ca0207bf08fad88b30574c479a
Davi Henrique dos Santos Silva <davi.silva2597@gmail.com>
2024-03-24 00:26:59 -0300

    Aula: Unidade 1 Parte 3

--- src/Components/MoviesList.js
+++ src/Components/MoviesList.js
@@ -3,7 +3,7 @@ import { MovieItem } from "./MovieItem";
 export const MoviesList = (props) => (
     <section>
         <ul>
-            {props.movies.map(m => <MovieItem title={m.title} />)}
+            {props.movies.map(m => <MovieItem key={m.title} title={m.title} />)}
         </ul>
     </section>
 )
